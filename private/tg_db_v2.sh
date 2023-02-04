5afcdc1ab29ef33fab3571a4d1b453e1140f68c0
voven4ek <batsura.vs@gmail.com>
2023-02-04 21:14:02 +0300

    Initial commit

---
+++ private/tg_db_v2.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/batsura-vs/dropBot_v2
+cd dropBot_v2 || echo "Error while downloading"
+pip3 install -r requirements.txt
+clear
+# shellcheck disable=SC2028
+echo "Success
+ Now you can run bot using this command:
+ python3 main.py"
