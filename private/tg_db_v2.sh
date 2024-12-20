ceb298f25e06bebcbca4fd7a30a51536d5eb6f14
Vladimir <batsura.vs@gmail.com>
2024-12-20 21:30:47 +0300

    Delete private/tg_db_v2.sh

--- private/tg_db_v2.sh
+++
@@ -1,8 +0,0 @@
-git clone https://github.com/batsura-vs/dropBot_v2
-cd dropBot_v2 || echo "Error while downloading"
-pip3 install -r requirements.txt
-# shellcheck disable=SC2028
-echo "Success
- Now you can run bot using this command:
- python3 main.py"
-rm -rf ../tg_db_v2.sh
